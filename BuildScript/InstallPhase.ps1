07bdc146c85621fcfafbeeca2beda862d7beb4c9
Lawrence Hwang <LawrenceHwang@users.noreply.github.com>
2018-02-18 12:23:51 -0800

    Add the awspowershell module

--- BuildScript/InstallPhase.ps1
+++ BuildScript/InstallPhase.ps1
@@ -8,4 +8,4 @@ $PSVersionTable
 Install-Module -Name psake -Force
 Install-Module -Name pester -Force
 Install-Module -Name PSScriptAnalyzer -Force
-# Install-Module -Name AWSPowerShell.netcore -Force
+Install-Module -Name AWSPowerShell.netcore -Force
